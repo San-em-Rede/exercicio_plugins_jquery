6b4a87c74fd66967676c35c09315fd19309f33d5
San-em-Rede <126499089+San-em-Rede@users.noreply.github.com>
2024-02-03 12:33:11 -0400

    Modificação #1

---
+++ main.js
@@ -0,0 +1,32 @@
+$(document).ready(function (){
+
+    $('#form').validate({
+
+        rules:{
+            name: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            telefone: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+        },
+        messages:{
+            name:'O campo Nome não pode estar vazio.',
+        }
+
+    })
+
+    $('#cep').mask('00000-000')
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+
+})
